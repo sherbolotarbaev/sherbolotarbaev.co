976144aaa1807531958f7ce026f4096b96059efb
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-05-05 02:08:22 +0600

    add: icons names

--- content/home/icons.tsx
+++ content/home/icons.tsx
@@ -8,22 +8,27 @@ import {
 type Icon = {
   svg: React.ReactElement;
   url: string;
+  name: string;
 };
 
 export const icons: Icon[] = [
   {
+    name: 'LinkedIn',
     svg: <BiLogoLinkedin size={20} />,
     url: 'https://www.linkedin.com/in/sherbolotarbaev',
   },
   {
+    name: 'GitHub',
     svg: <BiLogoGithub size={20} />,
     url: 'https://github.com/sherbolotarbaev',
   },
   {
+    name: 'Twitter',
     svg: <BiLogoTwitter size={20} />,
     url: 'https://twitter.com/sherbolotarbaev',
   },
   {
+    name: 'Instagram',
     svg: <BiLogoInstagram size={20} />,
     url: 'https://www.instagram.com/sherbolotarbaev',
   },
