a04328f51d355a683415d2de9c7e3ad7bd39b475
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-05-07 18:31:03 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,12 @@ const nextConfig = {
   experimental: {
     mdxRs: true,
   },
+  env: {
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
 };
 
 export default nextConfig;
