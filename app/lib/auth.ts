4900f5c454b18b47db4c25abe9cca4a98944a5ff
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-05-07 17:52:58 +0600

    update

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -8,16 +8,16 @@ export const {
 } = NextAuth({
   providers: [
     Github({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 
-  secret: process.env.NEXTAUTH_SECRET as string,
+  secret: process.env.NEXTAUTH_SECRET,
 
   pages: {
     signIn: '/sign-in',
