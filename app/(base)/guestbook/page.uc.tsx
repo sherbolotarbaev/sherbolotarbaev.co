90645e77a0fee9448c603143b5d76068192eaf4c
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-05-09 04:20:58 +0600

    update: guestbook loading

--- app/(base)/guestbook/page.uc.tsx
+++ app/(base)/guestbook/page.uc.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import type { User } from 'next-auth';
 
@@ -20,15 +20,34 @@ interface Props {
   user?: User;
 }
 
+function LoadingMessages({ count }: { count: number }) {
+  return (
+    <div className={`${scss.messages} ${scss.load}`}>
+      {count > 0 &&
+        Array.from({ length: count }).map(() => (
+          <div className={scss.message}>
+            <div className={scss.logo_wrapper}></div>
+
+            <div className={scss.info}>
+              <span className={scss.name}></span>
+
+              <span className={scss.text}></span>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
+
 export default function GuestbookClient({ user }: Readonly<Props>) {
   const [take, setTake] = useState<number>(7);
 
   const { data, isLoading, refetch, isFetching } = useGetGuestbookMessagesQuery({ take });
 
-  const handleLoadMore = () => {
+  const handleLoadMore = useCallback(() => {
     setTake((prevTake) => prevTake + 3);
     refetch();
-  };
+  }, [setTake, refetch]);
 
   const [open, setOpen] = useState<boolean>(false);
 
@@ -70,47 +89,45 @@ export default function GuestbookClient({ user }: Readonly<Props>) {
             </Button>
           )}
 
-          <div className={isLoading ? `${scss.messages} ${scss.load}` : scss.messages}>
-            {data && (
-              <>
-                {data.messages.map(({ message, image, name, createdAt }, index) => (
-                  <div key={index} className={scss.message}>
-                    {image && (
-                      <div className={scss.logo_wrapper}>
-                        <Image
-                          className={scss.logo}
-                          width={30}
-                          height={30}
-                          src={image}
-                          alt={name || 'User'}
-                          loading="lazy"
-                        />
-                      </div>
-                    )}
-
-                    <div className={scss.info}>
-                      <span className={scss.name}>
-                        {name.split(' ')[0]}
-                        <span className={scss.created_at}>• {formatDate(createdAt)}</span>
-                      </span>
-
-                      {message}
+          {data && !isLoading ? (
+            <div className={scss.messages}>
+              {data.messages.map(({ message, image, name, createdAt }, index) => (
+                <div key={index} className={scss.message}>
+                  {image && (
+                    <div className={scss.logo_wrapper}>
+                      <Image
+                        className={scss.logo}
+                        width={30}
+                        height={30}
+                        src={image}
+                        alt={name || 'User'}
+                        loading="lazy"
+                      />
                     </div>
+                  )}
+
+                  <div className={scss.info}>
+                    <span className={scss.name}>
+                      {name.split(' ')[0]}
+                      <span className={scss.created_at}>• {formatDate(createdAt)}</span>
+                    </span>
+
+                    {message}
                   </div>
-                ))}
+                </div>
+              ))}
 
-                {isFetching && !isLoading && (
-                  <div className={`${scss.messages} ${scss.load}`}></div>
-                )}
+              {isFetching && <LoadingMessages count={3} />}
 
-                {data.count !== data.totalCount && (
-                  <span className={scss.load_more} onClick={handleLoadMore}>
-                    Load more <BiChevronDown size={18} />
-                  </span>
-                )}
-              </>
-            )}
-          </div>
+              {data.count !== data.totalCount && (
+                <span className={scss.load_more} onClick={handleLoadMore}>
+                  Load more <BiChevronDown size={18} />
+                </span>
+              )}
+            </div>
+          ) : (
+            <LoadingMessages count={7} />
+          )}
         </div>
       </section>
     </>
