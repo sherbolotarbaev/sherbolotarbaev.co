a9432451169ebca835a964df0e82bdacebb8c1c6
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-06-19 19:48:20 +0600

    update: experience content

--- content/experience/jobs.tsx
+++ content/experience/jobs.tsx
@@ -12,19 +12,28 @@ export const jobs: Job[] = [
     date: '2023 - Present',
     role: 'Software Development Engineer',
     description: `
-    At WEDEVX, I collaborate with teams to develop robust backend solutions using technologies like <span>NestJS</span>, <span>Fastify</span>, <span>Node.js v20</span>, and <span>PostgreSQL</span>. I play a key role in integrating essential services such as <span>AWS Lambda</span> and <span>AWS IAM</span>. <br /> <br />
-    
-    One of my major accomplishments has been implementing quizzes on the platform, which generated over <span>800 leads</span> for free trials and ensured these leads were saved to the <span>CRM</span>. Through my optimization efforts, I achieved a <span>38%</span> improvement in our codebase <span>performance</span>. <br /> <br />
-    
-    I also work on <span>Soft Skills AI</span>, incorporating advanced AI features like speech-to-text, image recognition, and AWS S3 integration. Additionally, I'm enhancing our <span>DevXAI</span> feature, designed to help our students with debugging and assisting with exercises.`,
+    - Developed robust backend solutions using NestJS, Fastify, Node.js v20, and PostgreSQL.
+    - Integrated essential services such as AWS Lambda and AWS IAM.
+    - Implemented quizzes that generated 800+ leads, enhancing CRM data.
+    - Achieved a 38% performance improvement in the codebase through optimization efforts.
+    - Incorporated advanced AI features like speech-to-text, image recognition, and AWS S3 integration for Soft Skills AI.
+    - Enhanced the DevXAI feature to assist students with debugging and exercises.
+    - Designed and implemented microservices architecture using NestJS microservices and Kafka.js to ensure scalable and maintainable systems.
+    - Leveraged Large Language Models (LLM) to improve AI-driven features and user interactions.
+    - Conducted code reviews to ensure best practices and maintain high code quality.
+    - Utilized AWS and Google Cloud for deploying and managing cloud infrastructure, enhancing application scalability and reliability.
+    - Employed Docker for containerization, streamlining development workflows and ensuring consistency across environments.`,
     url: 'https://www.wedevx.co/',
   },
   {
     name: 'Mancho Devs',
     date: '2021 - 2023',
     role: 'Frontend Developer',
-    description:
-      'Developed adaptive and cross-platform websites using <span>React</span>, <span>Next.js</span>, <span>Redux</span>, <span>SCSS</span>, and <span>TypeScript</span>. <br /> <br /> Led the implementation of optimization strategies resulting in notable improvements in website <span>speed</span> and <span>performance</span>. <br /> <br /> Utilized advanced <span>caching</span> techniques to enhance user experience and page load times.',
+    description:`
+    - Developed adaptive and cross-platform websites using React, Next.js, Redux, SCSS, and TypeScript.
+    - Led optimization strategies, significantly improving website speed and performance.
+    - Utilized advanced caching techniques to enhance user experience and page load times.
+    - Conducted thorough code reviews to maintain code quality and foster team learning.`,
     url: 'https://www.mancho.dev/',
   },
 ];
